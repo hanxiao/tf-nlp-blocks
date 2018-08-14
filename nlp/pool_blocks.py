741645e1a67ffc7c272daa3e75e41a09c5a4e2f7
hanhxiao <hanhxiao@tencent.com>
2018-08-14 13:44:35 +0800

    add header

--- nlp/pool_blocks.py
+++ nlp/pool_blocks.py
@@ -10,6 +10,57 @@ from nlp.match_blocks import Transformer_match
 from nlp.nn import masked_reduce_max, masked_reduce_mean, get_var, minus_mask
 
 
+def SWEM_pool(seqs, mask, reduce='concat_mean_max', scope=None, reuse=None,
+              task_name=None, norm_by_layer=False, dropout_keep=1., **kwargs):
+    """
+    Simple word embedding fusion
+    """
+    with tf.variable_scope(scope or 'SWEM_Block', reuse=reuse):
+        if reduce == 'MAX':
+            pooled = masked_reduce_max(seqs, mask)
+        elif reduce == 'CONCAT_MEAN_MAX':  # often best
+            pooled = tf.concat([masked_reduce_mean(seqs, mask),
+                                masked_reduce_max(seqs, mask)], axis=1)
+        elif reduce == 'MEAN':  # often worst
+            pooled = masked_reduce_mean(seqs, mask)
+        elif reduce == 'LEVEL_MEAN_MAX':
+            # 1. segment sequence in a small window
+            # 2. do average pooling on each window
+            # 3. do max pooling across all windows
+            def reshape_seqs(x, avg_window_size=3, **kwargs):
+                B = tf.shape(x)[0]
+                L = tf.cast(tf.shape(x)[1], tf.float32)
+                D = x.get_shape().as_list()[-1]
+                b = tf.transpose(x, [0, 2, 1])
+                extra_pads = tf.cast(tf.ceil(L / avg_window_size) * avg_window_size - L, tf.int32)
+                c = tf.pad(b, tf.concat([tf.zeros([2, 2], dtype=tf.int32), [[0, extra_pads]]], axis=0))
+                return tf.reshape(c, [B, D, avg_window_size, -1])
+
+            # avg pooling with mask, be careful with all zero mask
+            d = tf.reduce_sum(reshape_seqs(seqs, **kwargs), axis=2) / \
+                tf.reduce_sum(reshape_seqs(tf.expand_dims(mask + 1e-10, axis=-1), **kwargs), axis=2)
+
+            # max pooling
+            pooled = tf.reduce_max(d, axis=2)
+        elif reduce == 'SELF_ATTENTION':
+            D = seqs.get_shape().as_list()[-1]
+            query = tf.tile(get_var('%s_query' % (task_name if task_name else ''),
+                                    shape=[1, D, 1]), [tf.shape(seqs)[0], 1, 1])
+            score = tf.nn.softmax(minus_mask(tf.matmul(seqs, query) / (D ** 0.5), mask), axis=1)  # [B,L,1]
+            pooled = tf.reduce_sum(score * seqs, axis=1)  # [B, D]
+        else:
+            raise NotImplementedError
+
+        pooled = tf.nn.dropout(pooled, keep_prob=dropout_keep)
+        if norm_by_layer:
+            pooled = tf.contrib.layers.layer_norm(pooled)
+
+        return pooled
+
+
+# the following blocks are for experimental purpose!!!
+# i just keep them here in case i need them
+
 def MH_Att_pool(seqs, mask, scope=None, reuse=None, **kwargs):
     with tf.variable_scope(scope or 'MultiHead_Attention_Pool_Block', reuse=reuse):
         rep = Transformer_match(seqs, seqs, **kwargs)
@@ -104,51 +155,3 @@ def MH_MR_CNN_pool(seqs, mask, num_heads=3,
         for j in range(num_heads):
             outputs.append(MR_CNN_pool(seqs, mask, scope='mr_cnn_block_%d' % j, **kwargs))
         return tf.add_n(outputs)
-
-
-def SWEM_pool(seqs, mask, reduce='concat_mean_max', scope=None, reuse=None,
-              task_name=None, norm_by_layer=False, dropout_keep=1., **kwargs):
-    """
-    Simple word embedding fusion
-    """
-    with tf.variable_scope(scope or 'SWEM_Block', reuse=reuse):
-        if reduce == 'MAX':
-            pooled = masked_reduce_max(seqs, mask)
-        elif reduce == 'CONCAT_MEAN_MAX':  # often best
-            pooled = tf.concat([masked_reduce_mean(seqs, mask),
-                                masked_reduce_max(seqs, mask)], axis=1)
-        elif reduce == 'MEAN':  # often worst
-            pooled = masked_reduce_mean(seqs, mask)
-        elif reduce == 'LEVEL_MEAN_MAX':
-            # 1. segment sequence in a small window
-            # 2. do average pooling on each window
-            # 3. do max pooling across all windows
-            def reshape_seqs(x, avg_window_size=3, **kwargs):
-                B = tf.shape(x)[0]
-                L = tf.cast(tf.shape(x)[1], tf.float32)
-                D = x.get_shape().as_list()[-1]
-                b = tf.transpose(x, [0, 2, 1])
-                extra_pads = tf.cast(tf.ceil(L / avg_window_size) * avg_window_size - L, tf.int32)
-                c = tf.pad(b, tf.concat([tf.zeros([2, 2], dtype=tf.int32), [[0, extra_pads]]], axis=0))
-                return tf.reshape(c, [B, D, avg_window_size, -1])
-
-            # avg pooling with mask, be careful with all zero mask
-            d = tf.reduce_sum(reshape_seqs(seqs, **kwargs), axis=2) / \
-                tf.reduce_sum(reshape_seqs(tf.expand_dims(mask + 1e-10, axis=-1), **kwargs), axis=2)
-
-            # max pooling
-            pooled = tf.reduce_max(d, axis=2)
-        elif reduce == 'SELF_ATTENTION':
-            D = seqs.get_shape().as_list()[-1]
-            query = tf.tile(get_var('%s_query' % (task_name if task_name else ''),
-                                    shape=[1, D, 1]), [tf.shape(seqs)[0], 1, 1])
-            score = tf.nn.softmax(minus_mask(tf.matmul(seqs, query) / (D ** 0.5), mask), axis=1)  # [B,L,1]
-            pooled = tf.reduce_sum(score * seqs, axis=1)  # [B, D]
-        else:
-            raise NotImplementedError
-
-        pooled = tf.nn.dropout(pooled, keep_prob=dropout_keep)
-        if norm_by_layer:
-            pooled = tf.contrib.layers.layer_norm(pooled)
-
-        return pooled
